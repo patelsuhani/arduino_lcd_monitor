b754deaf7f7a24c60ce0e69f56baabe703b81d0a
patelsuhani <121316408+patelsuhani@users.noreply.github.com>
2024-03-13 13:12:37 -0400

    debugged

--- arduino_lcd_monitor_src/arduino_lcd_monitor_src.ino
+++ arduino_lcd_monitor_src/arduino_lcd_monitor_src.ino
@@ -1,13 +1,13 @@
 #include <LiquidCrystal.h>
-LiquidCrystak lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 const int switchPin = 6;
 int switchState = 0;
 int prevSwitchState = 0;
 int reply;
 void setup() {
-  lcd begin(16, 2);
+  lcd.begin(16, 2);
   pinMode(switchPin, INPUT);
-  lcd print("Ask the");
+  lcd.print("Ask the");
   lcd.setCursor(0, 1);
   lcd.print("Crystal Ball!");
 }
