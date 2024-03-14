cb86bb8262f59bd6924a54c250d9053cd3669e43
patelsuhani <121316408+patelsuhani@users.noreply.github.com>
2024-03-13 22:25:43 -0400

    set up variables

--- arduino_lcd_scroll_src/arduino_lcd_scroll_src.ino
+++ arduino_lcd_scroll_src/arduino_lcd_scroll_src.ino
@@ -1,9 +1,9 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LiquidCrystal.h>
 
-}
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+const int switchPin = 6;
+int switchState = 0;
+int prevSwitchState = 0;
+int reply;
+int scrollPosition = 0; // Variable to keep track of the scrolling position
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
