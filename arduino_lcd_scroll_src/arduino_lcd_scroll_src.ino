49829412eb9268b93e7b385c1553e3884e60d80e
patelsuhani <121316408+patelsuhani@users.noreply.github.com>
2024-03-13 22:32:47 -0400

    added condition dependent output

--- arduino_lcd_scroll_src/arduino_lcd_scroll_src.ino
+++ arduino_lcd_scroll_src/arduino_lcd_scroll_src.ino
@@ -15,3 +15,47 @@ void setup() {
   lcd.print("Fruit Oracle!");
 }
 
+void loop() {
+  switchState = digitalRead(switchPin);
+  
+  if (switchState != prevSwitchState) {
+    if (switchState == LOW) {
+      reply = random(8);
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("The oracle says: ");
+      lcd.setCursor(0, 1);
+      
+      switch(reply){
+        case 0:
+          lcd.print("Apple ");
+          break;
+        case 1:
+          lcd.print("Banana ");
+          break;
+        case 2:
+          lcd.print("Cherry ");
+          break;
+        case 3:
+          lcd.print("Grapes ");
+          break;
+        case 4:
+          lcd.print("Orange ");
+          break;
+        case 5:
+          lcd.print("Kiwi ");
+          break;
+        case 6:
+          lcd.print("Mango ");
+          break;
+        case 7:
+          lcd.print("Lychee ");
+          break;
+      }
+      
+    }
+  }
+  
+  prevSwitchState = switchState;
+}
+
